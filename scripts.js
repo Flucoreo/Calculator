398319e42459807750d6069f836b6b875dbc47ef
Seth <142448427+Flucoreo@users.noreply.github.com>
2024-07-09 15:10:32 -0700

    Created basic add subtract multiply and divide functions in JS

---
+++ scripts.js
@@ -0,0 +1,17 @@
+// basic add, subtract, multiply, divide functions
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return Number((a - b).toFixed(4));
+}
+
+function divide(a, b){
+    return Number((a / b).toFixed(4));
+}
+
+function multiply(a, b){
+    return Number((a * b).toFixed(4));
+}
